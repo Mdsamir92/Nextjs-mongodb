f2b8052ccd2241185ef9833ad262c3a9a2874bdb
Mohammad Samir <87142605+Mdsamir92@users.noreply.github.com>
2023-12-12 14:47:19 +0530

    Update page.js

--- src/app/signup/page.js
+++ src/app/signup/page.js
@@ -1,13 +1,7 @@
 import SignupForm from '../components/SignupForm';
-import { getServerSession } from "next-auth";
-import { redirect } from "next/navigation";
-import { authOptions } from "../api/auth/[...nextauth]/route";
-
 
 export default async function SignupPage() {
-  const session = await getServerSession(authOptions);
 
-  if (session) redirect("/signup");
   return (
     <div>
  
